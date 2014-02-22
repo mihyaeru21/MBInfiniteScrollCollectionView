fc2d588b3cf43ccf66747bf149271f6ba2adeee1
Mihyaeru <mihyaeru.app@gmail.com>
2014-02-22 17:03:39 +0900

    UICollectionViewControllerでも初期位置がずれないように修正 issue #4 に対応

---
+++ MBInfiniteScrollCollectionViewDemo/MBInfiniteScrollCollectionViewDemo/MBCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBCollectionViewController.h
+//  MBInfiniteScrollCollectionViewDemo
+//
+//  Created by Mihyaeru on 2013/12/31.
+//  Copyright (c) 2013年 Mihyaeru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBCollectionViewController : UICollectionViewController
+
+@end
